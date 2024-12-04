5d357f5226655072d7f186544ffe9b33715c884f
fthanb <famukh16@gmail.com>
2024-12-04 13:38:47 +0700

    update Functions

--- Dataset/Functions/dataset.py
+++ Dataset/Functions/dataset.py
@@ -1,11 +1,11 @@
-### Sesuaikan sama lokasi dataset cloud
+## Sesuaikan sama lokasi dataset cloud
 
-# # Baca Data dari CSV
-# import pandas as pd
+# Baca Data dari CSV
+import pandas as pd
 
-# recipes_df = pd.read_csv("recipes_with_final_embeddings.csv")
-# pd.set_option('display.max_colwidth', None)
+recipes_df = pd.read_csv("recipes_with_final_embeddings.csv")
+pd.set_option('display.max_colwidth', None)
 
-# def get_recipes_df():
-#     return recipes_df
+def get_recipes_df():
+    return recipes_df
 
--- Dataset/Functions/main.py
+++ Dataset/Functions/main.py
@@ -1,16 +1,16 @@
-# """FITUR 1"""
-# from user_ingredient import recipe_ingredient
-# recipe_ingredient()
-# """==========================================="""
+"""FITUR 1"""
+from user_ingredient import recipe_ingredient
+recipe_ingredient()
+"""==========================================="""
 
 
 
 """FITUR 2"""
 # CONTOH KASUS
-weight = 70  # kg
-height = 175  # cm
-age = 25  # years
-gender = 1  # Male (2 for Female)
+weight = 70         # kg
+height = 175        # cm
+age = 25            # years
+gender = 1          # Male (2 for Female)
 activity_level = 3  # Moderately active (Range 1-5)
 
 from user_health import calories
